0a55d42c8b5cc3c1db66ab5f509b4767fd58f321
DCL <intesarulhaquey4@gmail.com>
2021-04-16 01:58:35 +0600

    1st commit

--- app/src/main/java/com/example/foodorder/OrderActivity.java
+++ app/src/main/java/com/example/foodorder/OrderActivity.java
@@ -6,14 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] foodarr = {"Select", "Cheese Burger", "Beef Burger", "Large Pizza", "Kacchi", "Sandwich"};
 
-    EditText foodName, priceTK;
+    Spinner myspinner;
+    EditText priceTK;
     RadioGroup myradiogroup;
     RadioButton myradiobutton;
     Button order_button;
@@ -24,7 +30,13 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        foodName = findViewById(R.id.food_nameID);
+        myspinner = (Spinner) findViewById(R.id.food_nameID);
+        myspinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter array = new ArrayAdapter(this, android.R.layout.simple_spinner_item, foodarr);
+        array.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(array);
+
         priceTK = findViewById(R.id.priceID);
         myradiogroup = findViewById(R.id.myradiogroup);
         order_button = findViewById(R.id.order_button);
@@ -32,15 +44,44 @@ public class OrderActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 MyDatabaseHelper myDB = new MyDatabaseHelper(OrderActivity.this);
-                food = foodName.getText().toString().trim();
+                food = myspinner.getSelectedItem().toString();
                 price = priceTK.getText().toString().trim();
                 int radiobuttonid = myradiogroup.getCheckedRadioButtonId();
                 myradiobutton = findViewById(radiobuttonid);
                 quantity = myradiobutton.getText().toString().trim();
+
                 myDB.addOrder(food, price, quantity);
                 Intent i = new Intent(OrderActivity.this, MainActivity2.class);
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        if (text.equals("Cheese Burger")) {
+            priceTK.setText("150");
+        }
+        if (text.equals("Beef Burger")) {
+            priceTK.setText("200");
+        }
+        if (text.equals("Large Pizza")) {
+            priceTK.setText("500");
+        }
+        if (text.equals("Kacchi")) {
+            priceTK.setText("400");
+        }
+        if (text.equals("Sandwich")) {
+            priceTK.setText("100");
+        }
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
